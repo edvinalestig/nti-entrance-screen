855c57e2b4fcea252fbf25cd7a0e11f5557b4f52
itggot-edvin-alestig <edvin.alestig@itggot.se>
2019-01-11 19:57:45 +0100

    Added västtrafik module

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 # coding: utf-8
 from flask import Flask, render_template
-import jinja2
+import vasttrafik
+
 app = Flask(__name__)
+
+with open("creds.txt") as f:
+    key, secret = f.readlines()
+
+vt = vasttrafik.Reseplaneraren(key.strip(), secret.strip(), 1)
 
 @app.route("/")
 def index():
-    return render_template("template.html", test="Hi")
+    stop_id = vt.location_name(input="Chalmers").get("LocationList").get("StopLocation")[0].get("id")
+    return render_template("template.html", test=stop_id)
